60c0ddd8b3b4f02c0a73451efd0746ef09fb9ebb
Sam James <samjojames@gmail.com>
2022-10-15 16:48:02 +0100

    implement simple react form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./Pages/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,5 +1,8 @@
 import { Box } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import "./Login.css";
 
 type Inputs = {
   example: string;
@@ -7,32 +10,60 @@ type Inputs = {
 };
 
 export default function App() {
+  const [formData, setFormData] = useState({});
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-  console.log(watch("example")); // watch input value by passing the name of it
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    setFormData({
+      data,
+    });
+    await axios.post("cheapskate.pythonanywhere.com/login", {
+      data,
+    });
+  };
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", maxWidth: "30vw" }}>
-      <form onSubmit={handleSubmit(onSubmit)} className="login-form">
-        {/* register your input into the hook by invoking the "register" function */}
-        <input
-          defaultValue="username"
-          {...(register("example"), { required: true })}
-        />
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+      className="login-page"
+    >
+      <h1>Welcome to cheapskate</h1>
+      <Box sx={{ marginTop: "25em" }} gap={4}>
+        <form onSubmit={handleSubmit(onSubmit)} className="login-form">
+          {/* register your input into the hook by invoking the "register" function */}
+          <input
+            type="text"
+            placeholder="username"
+            {...(register("example"), { required: true })}
+            className="input"
+          />
 
-        {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("exampleRequired", { required: true })} />
-        {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
+          {/* include validation with required or other standard HTML validation rules */}
+          <input
+            type="password"
+            placeholder="password"
+            {...register("exampleRequired", { required: true })}
+            className="input"
+          />
+          {/* errors will return when field validation fails  */}
+          {errors.exampleRequired && <span>This field is required</span>}
 
-        <input type="submit" />
-      </form>
+          <input type="submit" />
+        </form>
+      </Box>
     </Box>
   );
 }
